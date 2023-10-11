e93aee8ddc91fdcd529263cf561184169ab1b787
lukas <lukas_skyry_14@hotmail.cl>
2023-10-11 19:48:07 -0300

    json

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -16,11 +19,20 @@ const (
 	dbname   = "bdtest"
 )
 
+var db *sql.DB
+
+type Aeropuerto struct {
+	Column1 string `json:"Column1"`
+	Column2 string `json:"Column2"`
+	Column3 string `json:"Column3"`
+}
+
 func main() {
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := sql.Open("postgres", connStr)
+	var err error
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,26 +45,45 @@ func main() {
 
 	fmt.Println("Conexión exitosa")
 
-	// solo para mostrar xD
+	r := mux.NewRouter()
+	r.HandleFunc("/aeropuertos", GetAeropuertos).Methods("GET")
 
+	http.Handle("/", r)
+
+	fmt.Println("Servidor escuchando en el puerto 8080...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func GetAeropuertos(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * from Aeropuerto")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
-	fmt.Printf("%-8s %-40s %s\n", "Column1", "Column2", "Column3")
+	var aeropuertos []Aeropuerto
 
 	for rows.Next() {
-		var column1, column2, column3 string
-		if err := rows.Scan(&column1, &column2, &column3); err != nil {
+		var a Aeropuerto
+		if err := rows.Scan(&a.Column1, &a.Column2, &a.Column3); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%-8s %-40s %s\n", column1, column2, column3)
+		aeropuertos = append(aeropuertos, a)
 	}
 
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	// Convertir el resultado en JSON
+	jsonData, err := json.Marshal(aeropuertos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Configurar las cabeceras HTTP para indicar que la respuesta es JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Enviar el JSON como respuesta
+	w.Write(jsonData)
 }
